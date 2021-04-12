ffdb4ad4167bc8c0e0ceb0c3b3cf410fb8070bac
Aleksey Shysh <setevoybog@gmail.com>
2021-04-12 17:50:39 +0300

    version 0.503

--- task3/src/main/java/by/alekseyshysh/task3/warehouse/Warehouse.java
+++ task3/src/main/java/by/alekseyshysh/task3/warehouse/Warehouse.java
@@ -5,28 +5,43 @@ import java.util.Map;
 
 public class Warehouse {
 
-	//TODO implement class
+	// TODO implement class
 	// Площади, Объемы,Периметры фигур должны храниться в объекте класса-Warehouse
-	// Любое изменение параметра фигуры должно вызывать пересчет соответствующих значений 
-	// в классе-Warehouse. Для решения данной задачи использовать паттерны 
-	// Observer(можно использовать FlowAPI) и 
-	// 	Singleton(потокобезопасные варианты использовать запрещено).
-	
+	// Любое изменение параметра фигуры должно вызывать пересчет соответствующих
+	// значений
+	// в классе-Warehouse. Для решения данной задачи использовать паттерны
+	// Observer(можно использовать FlowAPI) и
+	// Singleton(потокобезопасные варианты использовать запрещено).
+
 	private static final Warehouse instance = new Warehouse();
 	private final Map<Long, RegularPolygonParameter> regularPolygonMap = new HashMap<>();
 	private final Map<Long, RegularPyramidParameter> regularPyramidMap = new HashMap<>();
-	
+
 	private Warehouse() {
 	}
-	
+
 	public static Warehouse getInstance() {
 		return instance;
 	}
-	
-	public void putParametersPyramid(long id, double area, double perimeter) {
+
+	public void putParametersRegularPyramid(long id, double area, double perimeter) {
+	}
+
+	public void putParametersRegularPolygon(long id, double sideFacesArea, double volume) {
+	}
+
+	public void updateParametersRegularPyramid(long id, double area, double perimeter) {
+	}
+
+	public void updateParametersRegularPolygon(long id, double sideFacesArea, double volume) {
+	}
+
+	public boolean containsKeyRegularPyramid(Long key) {
+		return regularPyramidMap.containsKey(key);
 	}
 	
-	public void putParametersPolygon(long id, double sideFacesArea, double volume) {
+	public boolean containsKeyRegularPolygon(Long key) {
+		return regularPolygonMap.containsKey(key);
 	}
-	
+
 }
